295f94294ef4163a1d401823abd0c5b8c5125944
CooperCarnahan <coopcarn@gmail.com>
2021-11-08 09:45:39 -0600

    Fix cmake build errors. Got a little distracted

---
+++ src/foo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int foo(int x){
+  return x*2;
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <foo.h>
 
 int main(int argc, char *argv[])
 {
+  if (argc > 1){
+    printf("Foo generated the number: %d\n", foo(atoi(argv[1]))); 
+  }
   return 0;
 }
